13cdeeaa4ec20d46dc3443efe49e0441194f5f3d
helidesenvolve <helidesenvolve@gmail.com>
2020-04-24 22:24:53 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,8 +25,8 @@ public class ChessMatch { //partida de xadrez (sera o coração do nosso pragrama)
 	}
 	
 	private void initialSetup(){
-		board.placePiece(new Rook (board, Color.WHITE),new Position (2 ,1));
-		board.placePiece(new King(board, Color.BLACK), new Position (0, 4));
+		board.placePiece(new Rook (board, Color.WHITE),new Position (0 ,1));
+		board.placePiece(new King(board, Color.BLACK), new Position (7, 4));
 		board.placePiece(new King (board, Color.WHITE), new Position (7, 4));
 		
 	}
